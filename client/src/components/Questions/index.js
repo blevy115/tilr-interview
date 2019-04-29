0b8ceb8cc9cd8beae0e71b9460e03de181cd6873
Brandon Levy <brandon.levy@mail.mcgill.ca>
2019-04-28 21:19:08 -0400

    Add link to sign up page

--- client/src/components/Questions/index.js
+++ client/src/components/Questions/index.js
@@ -11,6 +11,7 @@ const Questions = () => (
       <ul className='nav nav-pills questions__nav'>
         <li className='nav-item'><NavLink to='/' className='nav-link' exact>Home</NavLink></li>
         <li className='nav-item'><NavLink to='/create' className='nav-link'>Create</NavLink></li>
+        <li className='nav-item'><NavLink to='/login' className='nav-link'>Sign Up</NavLink></li>
       </ul>
     </div>
     <Switch>
