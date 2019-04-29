ff8bac175b56ea27369f53a6452e51a6de8175bf
Brandon Levy <brandon.levy@mail.mcgill.ca>
2019-04-28 21:08:49 -0400

    fixed question tag

--- api/seeds/001 - questions.js
+++ api/seeds/001 - questions.js
@@ -1,6 +1,6 @@
 exports.seed = async (knex) => {
   await knex.batchInsert('questions', [
-    { text: 'Are the Leafs ever going to win the Stanley Cup again?' , tag:'hocke'},
+    { text: 'Are the Leafs ever going to win the Stanley Cup again?' , tag:'hockey'},
     { text: 'Is pineapple an appropriate pizza topping?', tag:'pizza' },
     { text: 'Is beer an appropriate breakfast drink?', tag:'beer'}
   ])
